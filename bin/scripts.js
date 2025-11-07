02b49a0478aca88178d6624fbfdf05829a747423
wurducius <wurducius@gmail.com>
2025-11-07 21:17:08 +0100

    Add forgotten start script to bin

--- bin/scripts.js
+++ bin/scripts.js
@@ -4,7 +4,7 @@ const scriptPath = join(__dirname, "..", "src")
 
 const script = (id) => ({ cmd: id, run: `node ${join(scriptPath, `${id}.js`)}` })
 
-const scriptIds = ["build", "clean", "lighthouse", "reinstall", "serve"]
+const scriptIds = ["start", "build", "clean", "lighthouse", "reinstall", "serve"]
 
 const SCRIPTS = scriptIds.map(script)
 
